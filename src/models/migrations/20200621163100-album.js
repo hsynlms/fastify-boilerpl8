41540be1e60337eefe23c3499ca67d1665d0b37f
Huseyin ELMAS <hsynlms47@gmail.com>
2020-06-24 17:11:48 +0300

    add some migrations

---
+++ src/models/migrations/20200621163100-album.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// to be able to use esm modules with commonjs
+require = require('esm')(module) // eslint-disable-line no-global-assign
+
+// import required local modules
+const { AlbumModel } = require('../index')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // get the model
+    const model = AlbumModel.model(queryInterface.sequelize, Sequelize.Model, Sequelize.DataTypes)
+
+    return queryInterface.createTable(
+      model.name,
+      model.rawAttributes,
+      model.options,
+      model
+    )
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(AlbumModel.name)
+  }
+}
