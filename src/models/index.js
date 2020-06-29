04f34e37023972f1c66865c5f7081e366c701498
Huseyin ELMAS <hsynlms47@gmail.com>
2020-06-29 17:10:34 +0300

    add model container

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// import required local modules
+import AlbumModel from './albumModel'
+import SingerModel from './singerModel'
+
+// gather all models to be used in anywhere in the application and
+// export the models
+export {
+  AlbumModel,
+  SingerModel
+}
