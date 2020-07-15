1876d247bd05cc8c4df3e70fd00c981d0cdb0d53
Huseyin ELMAS <hsynlms47@gmail.com>
2020-07-15 19:51:37 +0300

    fix controller registrar call issues

--- src/index.js
+++ src/index.js
@@ -9,27 +9,24 @@ import configurations from './configurations'
 import constants from './constants'
 import pluginRegistrar from './infrastructure/pluginRegistrar'
 import controllerRegistrar from './infrastructure/controllerRegistrar'
-import { routeRegistrar as app, routes } from './infrastructure/routeRegistrar'
+import routeRegistrar from './infrastructure/routeRegistrar'
+
+// create a fastify instance with custom options
+const fastify = _({ logger: false })
+
+// log errors
+fastify.addHook('onError', async (req, reply, error) => {
+  // exception handler middleware
+  // can be set here (like Sentry)
+})
 
 // register controllers
-controllerRegistrar(app)
+controllerRegistrar(fastify, routeRegistrar)
   .then(() => {
     // controller registration is done
 
-    // create a fastify instance with custom options
-    const fastify = _({ logger: false })
-
-    // log errors
-    fastify.addHook('onError', async (req, reply, error) => {
-      // exception handler middleware
-      // can be set here (like Sentry)
-    })
-
     // register plugins
     pluginRegistrar(fastify)
-
-    // register routes
-    routes.map(route => fastify.route(route))
 
     // make fastify listen the determined port number to handle incoming requests
     fastify.listen(configurations[constants.environment].webapi.port, (err, address) => {
